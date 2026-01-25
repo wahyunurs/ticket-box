af0adfa07d6bf9a32a3ffe4ddf2f5fed255414a9
Wahyu NurS <whyofficial25032004@gmail.com>
2026-01-25 23:58:00 +0700

    feat: add seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kategori;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $kategoris = [
+            ['nama_kategori' => 'Konser'],
+            ['nama_kategori' => 'Seminar'],
+            ['nama_kategori' => 'Workshop'],
+        ];
+
+        foreach ($kategoris as $kategori) {
+            Kategori::create(['nama' => $kategori['nama_kategori']]);
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CategorySeeder::class,
+            UserSeeder::class,
+            EventSeeder::class,
+            TicketSeeder::class,
+            OrderSeeder::class,
         ]);
     }
 }
